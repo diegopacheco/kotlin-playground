1160855b5f8f494d37c0373f4841548395be4de8
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-10 01:27:14 -0300

    Added defautl functions

--- lang-basics-101/src/main/kotlin/functions/Functions.kt
+++ lang-basics-101/src/main/kotlin/functions/Functions.kt
@@ -10,8 +10,14 @@ fun printSumV3(a: Int, b: Int): Unit {
     println("sumV3 of $a and $b is ${a + b}")
 }
 
+fun fooDefaults(a: Int = 0, b: String = "") {
+    println("a = ${a} b = ${b}")
+}
+
+
 fun main():Unit {
     println("sumV1 1 +1 = " + sumV1(1,1))
     println("sumV2 1 +1 = " + sumV2(1,1))
     printSumV3(1,1)
+    fooDefaults()
 }
