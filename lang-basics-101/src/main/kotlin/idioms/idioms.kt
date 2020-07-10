27545c139a5623ca5cc17540575a0feab8e799fb
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-10 01:40:37 -0300

    adding more kotlin idioms

--- lang-basics-101/src/main/kotlin/idioms/idioms.kt
+++ lang-basics-101/src/main/kotlin/idioms/idioms.kt
@@ -10,4 +10,35 @@ fun main() {
     println(p)
     println(p.email)
     println(p.equals(p))
+
+    val list = listOf(-1,2,-3,4,5,-1,0,-8,-10)
+    val positives = list.filter { x -> x > 0 }
+    println("Filter positives ${positives}")
+
+    val positivesV2 = list.filter { it > 0 }
+    println("Filter positivesV2 ${positivesV2}")
+
+    if (2 in list) {
+        println("2 is here")
+    }
+    if (-11 !in list) {
+        println("-11 not here")
+    }
+
+    val map = mapOf("RS" to "POA","SC" to "Floripa")
+    for ((k, v) in map) {
+        println("$k -> $v")
+    }
+    println(map["RS"])
+
+    val l:String by lazy {
+        println("this is lazy prop - runnign now... ")
+        "Lazy P"
+    }
+    println("something else...")
+    println("$l")
+
+    fun String.to42():String = "Forget original string: 42"
+    println("Convert this to camelcase".to42())
+
 }
