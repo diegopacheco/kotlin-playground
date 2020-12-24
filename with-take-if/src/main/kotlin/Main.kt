f475aa4bfc77e5490c87d3060dee987b3ea4507e
diegopacheco <diego.pacheco.it@gmail.com>
2020-12-24 12:17:09 -0300

    kotlin with and takeif

---
+++ with-take-if/src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+
+fun main(args:Array<String>) {
+  println("TakeIf ${displaySubstringPosition("this is it","it")}")
+
+  // With
+  val numbers = mutableListOf("one", "two", "three")
+  with(numbers) {
+    val firstItem = first()
+    val lastItem = last()
+    println("First item: $firstItem, last item: $lastItem")
+  }
+}
+
+fun displaySubstringPosition(input: String, sub: String) {
+  input.indexOf(sub).takeIf { it >= 0 }?.let {
+    println("The substring $sub is found in $input.")
+    println("Its start position is $it.")
+  }
+}
