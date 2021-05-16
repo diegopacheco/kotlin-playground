a0014af094bc61bcddee0bf38d9f4fe869acf71b
diegopacheco <diego.pacheco.it@gmail.com>
2021-05-16 19:29:04 -0300

    fun with parsix

--- parsix-fun/src/main/kotlin/Main.kt
+++ parsix-fun/src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
+import parsix.core.Ok
+import parsix.core.TerminalError
+import parsix.core.Parsed
+
+data class Age(val value: UInt)
+data class AdultAge(val value: UInt)
+data class NotAdultError(val inp: Age) : TerminalError
+
+fun parseAdultAge(inp:Age): Parsed<AdultAge> =
+  if (inp.value >= 18u)
+    Ok(AdultAge(inp.value))
+  else
+    NotAdultError(inp)
 
 fun main(args:Array<String>) {
-  println("Hello World")
+  println("Adult? 37 == " + parseAdultAge(Age(37u)))
+  println("Adult? 17 == " + parseAdultAge(Age(17u)))
 }
