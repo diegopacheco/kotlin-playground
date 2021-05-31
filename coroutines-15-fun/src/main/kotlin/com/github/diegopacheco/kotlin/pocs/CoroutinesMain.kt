7a60962fc2efd60ac97ff0d82886fb641a22fdf9
diegopacheco <diego.pacheco.it@gmail.com>
2021-05-31 18:55:04 -0300

    coroutines

--- coroutines-15-fun/src/main/kotlin/com/github/diegopacheco/kotlin/pocs/CoroutinesMain.kt
+++ coroutines-15-fun/src/main/kotlin/com/github/diegopacheco/kotlin/pocs/CoroutinesMain.kt
@@ -1,6 +1,8 @@
 package com.github.diegopacheco.kotlin.pocs
 
 import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 suspend fun main() = coroutineScope {
     launch {
@@ -8,4 +10,78 @@ suspend fun main() = coroutineScope {
         println("Kotlin Coroutines World!")
     }
     println("Hello")
+    buildSequenceFun()
+    expensive()
+    withTimeout()
+    runAsyncComputations()
+}
+
+suspend fun buildSequenceFun() {
+    val fibonacciSeq = sequence<Int> {
+        var a = 0
+        var b = 1
+        yield(1)
+        while (true) {
+            yield(a + b)
+            val tmp = a + b
+            a = b
+            b = tmp
+        }
+    }
+    val res = fibonacciSeq
+        .take(5)
+        .toList()
+    println("res $res")
+}
+
+suspend fun expensive(){
+    runBlocking<Unit> {
+        val counter = AtomicInteger(0)
+        val numberOfCoroutines = 300_000
+        val jobs = List(numberOfCoroutines) {
+            launch {
+                delay(1000L)
+                counter.incrementAndGet()
+            }
+        }
+        jobs.forEach { it.join() }
+        assert(counter.get() == numberOfCoroutines)
+        print("WOW created $numberOfCoroutines with no OutOfMemoryException!")
+    }
+}
+
+fun withTimeout(){
+    runBlocking<Unit> {
+        withTimeout(3000L) {
+            repeat(2) { i ->
+                println("Some expensive computation $i ...")
+                delay(500L)
+            }
+        }
+    }
+}
+
+
+fun runAsyncComputations(){
+    runBlocking<Unit> {
+        val delay = 3000L
+        val time = measureTimeMillis {
+            val one = async {
+                someExpensiveComputation(delay)
+            }
+            val two = async{
+                someExpensiveComputation(delay)
+            }
+            runBlocking {
+                one.await()
+                two.await()
+            }
+        }
+        assert(time < delay * 2)
+        println("time $time")
+    }
+}
+
+suspend fun someExpensiveComputation(delayTime:Long) {
+    delay(delayTime)
 }
