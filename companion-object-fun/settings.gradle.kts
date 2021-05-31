cb2444a2339aec51dbba54a1581aee5ee5ea5e77
diegopacheco <diego.pacheco.it@gmail.com>
2021-05-31 18:24:37 -0300

    kotlin companion object - static field

---
+++ companion-object-fun/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "companion-object-fun"
+
---
+++ companion-object-fun/src/main/kotlin/CompanionObjectMain.kt
@@ -0,0 +1,10 @@
+class Person {
+    companion object {
+        val NAME_KEY = "name_key"
+    }
+}
+
+fun main() {
+    val key = Person.NAME_KEY
+    println("key: $key")
+}
