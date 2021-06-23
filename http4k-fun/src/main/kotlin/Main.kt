933bc73703b205c2adb852bdb46c2d9c0a472b0d
diegopacheco <diego.pacheco.it@gmail.com>
2021-06-23 03:13:17 -0300

    funb with http4k

---
+++ http4k-fun/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+import org.http4k.client.ApacheClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.server.Netty
+import org.http4k.server.asServer
+
+fun main(args:Array<String>) {
+  val app = { request: Request -> Response(OK).body("Hello, ${request.query("name")}!") }
+  val server = app.asServer(Netty(9000)).start()
+  val client = ApacheClient()
+  val request = Request(Method.GET, "http://localhost:9000").query("name", "John Doe")
+  println(client(request))
+  server.stop()
+}
