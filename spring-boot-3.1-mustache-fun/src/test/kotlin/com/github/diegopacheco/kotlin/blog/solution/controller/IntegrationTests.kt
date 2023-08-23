56470c4f5cd3eb34a006b68d9997fbc122cd71e8
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-22 23:53:54 -0700

    working

--- spring-boot-3.1-mustache-fun/src/test/kotlin/com/github/diegopacheco/kotlin/blog/solution/controller/IntegrationTests.kt
+++ spring-boot-3.1-mustache-fun/src/test/kotlin/com/github/diegopacheco/kotlin/blog/solution/controller/IntegrationTests.kt
@@ -3,12 +3,15 @@ package com.github.diegopacheco.kotlin.blog.solution.controller
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    classes = [HtmlController::class])
+@EnableAutoConfiguration
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
