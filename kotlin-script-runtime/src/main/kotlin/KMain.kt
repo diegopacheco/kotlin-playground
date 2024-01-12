84f44981aeb681d32b676cf98fba7575b19988a7
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-11 21:46:33 -0800

    kotlin

--- kotlin-script-runtime/src/main/kotlin/KMain.kt
+++ kotlin-script-runtime/src/main/kotlin/KMain.kt
@@ -1,3 +1,12 @@
+import javax.script.*
+
 fun main(args:Array<String>) {
     println("Hello world Kotlin!")
+
+    val engine = ScriptEngineManager().getEngineByExtension("kts")!!
+    val bindings = SimpleBindings()
+    bindings.put("extra",5);
+    engine.setBindings(bindings, ScriptContext.GLOBAL_SCOPE)
+
+    println (engine.eval("2 + 3 + extra"))
 }
