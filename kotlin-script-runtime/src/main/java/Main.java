41df915fd7699e8256d97ac997fa2ed36d399603
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-11 21:56:00 -0800

    kotlin

--- kotlin-script-runtime/src/main/java/Main.java
+++ kotlin-script-runtime/src/main/java/Main.java
@@ -1,8 +1,13 @@
+import javax.script.*;
 import java.io.IOException;
 
 public class Main{
   public static void main(String args[]) throws Exception {
-    String cmd = " echo 'println(42)' | script.kts - ";
-    Runtime.getRuntime().exec(cmd);
+    ScriptEngine engine = new ScriptEngineManager().getEngineByExtension("kts");
+    Bindings bindings = new SimpleBindings();
+    bindings.put("extra",5);
+    engine.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
+
+    System.out.println(engine.eval("2 + 3 + extra"));
   }
 }
