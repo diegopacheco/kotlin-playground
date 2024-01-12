e8bc6f7bff447e4ecb8469d209c9f1f064751822
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-11 21:26:35 -0800

    kotlin

--- kotlin-script-runtime/src/main/java/Main.java
+++ kotlin-script-runtime/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main{
   public static void main(String args[]) throws Exception {
-    String cmd = " println(42) > script.kts | kotlinc -script ";
+    String cmd = " echo 'println(42)' > script.kts | sh ";
     Runtime.getRuntime().exec(cmd);
   }
 }
