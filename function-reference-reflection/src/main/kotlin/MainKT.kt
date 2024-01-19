be05cf3ff05bbd7e99e22010fc5c86762eb2cbbc
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-19 00:47:31 -0800

    kotlin poc - reference

---
+++ function-reference-reflection/src/main/kotlin/MainKT.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>) {
+    fun isOdd(x: Int) = x % 2 != 0
+    fun isOdd(s: String) = s == "brillig" || s == "slithy" || s == "tove"
+
+    val numbers = listOf(1, 2, 3)
+    println(numbers.filter(::isOdd)) // refers to isOdd(x: Int)
+}
