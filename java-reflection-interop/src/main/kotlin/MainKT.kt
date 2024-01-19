eaadfd8ff38017434089b1946d320138c253a289
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-19 00:47:14 -0800

    kotlin poc - property

---
+++ java-reflection-interop/src/main/kotlin/MainKT.kt
@@ -0,0 +1,8 @@
+import kotlin.reflect.jvm.*
+
+class A(val p: Int)
+
+fun main() {
+    println(A::p.javaGetter) // prints "public final int A.getP()"
+    println(A::p.javaField)  // prints "private final int A.p"
+}
