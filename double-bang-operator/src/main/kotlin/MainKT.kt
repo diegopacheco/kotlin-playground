3684a3c1c8d043d5678accba3ee19aca11f12c94
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-18 00:48:06 -0800

    kotlin poc

--- double-bang-operator/src/main/kotlin/MainKT.kt
+++ double-bang-operator/src/main/kotlin/MainKT.kt
@@ -1,3 +1,5 @@
+import kotlin.text.toLowerCase
+
 /**
  * https://stackoverflow.com/questions/34342413/what-is-the-kotlin-double-bang-operator
  * 
@@ -12,7 +14,17 @@ fun main(args: Array<String>) {
   //println(email!!) // The result of the expression is always nullkotlin(ALWAYS_NULL)
   
   var str :String? = "Foo"
-  str = null
+  // uncomment str and you will have a problem
+  //str = null 
   // Unresolved reference: lowerCasekotlin(UNRESOLVED_REFERENCE)
-  //val lowerCase = str!!.lowerCase() 
+  val lowerCase = str!!.lowercase()
+  println(lowerCase)
+
+  val willWork :String? = "works"
+  printMe(willWork)
+}
+
+fun printMe(str: String?): Unit {
+  val lowerCase = str!!.lowercase()
+  println(lowerCase)
 }
