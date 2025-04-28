b4ae1c790902ca93139ba39e85d9db752432620d
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-27 18:40:50 -0700

    typeclass kotlin - v3

--- typeclass/src/main/kotlin/TypeSystem.kt
+++ typeclass/src/main/kotlin/TypeSystem.kt
@@ -1,3 +1,6 @@
+import arrow.core.Either
+import arrow.core.EitherNel
+
 object Storage {
     val users = listOf("user-1234", "user-444")
     val stocks = listOf("GOOG", "TESLA", "RTKC")
@@ -69,33 +72,42 @@ object EcommerceV2 {
             is Validatable.CreatePortfolio -> {
                 if (!Storage.users.contains(event.userId)) Unit
                 else if (event.amount <= 0) Unit
-                else println("New Portfolio created for user " +
+                else println(
+                    "New Portfolio created for user " +
                             "${event.userId} with amount" +
-                            " ${event.amount}")
+                            " ${event.amount}"
+                )
             }
+
             is Validatable.ChangePortfolio -> {
                 if (!Storage.users.contains(event.userId)) Unit
                 else if (!Storage.stocks.contains(event.stock)) Unit
-                else println("Making Portfolio change for user " +
+                else println(
+                    "Making Portfolio change for user " +
                             "${event.userId} with amount" +
-                            " ${event.stock}")
+                            " ${event.stock}"
+                )
             }
         }
     }
 }
 
-object EcommerceV3{
-    data class ValidationError(val message:String)
-    interface Validatable<T>{
-        fun check(): Either<ValidationError,T>
+object EcommerceV3 {
+    data class ValidationError(val message: String)
+    interface Validatable<T> {
+        fun check(): EitherNel<ValidationError, T>
     }
 
     data class CreatePortfolio(
-        val userid:String,
+        val userid: String,
         val amount: Double
-    ): Validatable<CreatePortfolio>{
-        override fun check(): Either<ValidationError, CreatePortfolio> {
-            TODO("Not yet implemented")
-        }
+    ) : Validatable<CreatePortfolio> {
+        override fun check(): EitherNel<ValidationError, CreatePortfolio> =
+            EitherNel.zipOrAccumulate(
+                if (!Storage.users.contains(userid)) Either.Left(ValidationError( "Error users $userid not found"))
+                else Either.Right(this),
+                if (amount <= 0.0) Either.Left(ValidationError("Error amount $amount is negative"))
+                else Either.Right(this)
+            ) { a, _ -> a }
     }
 }
